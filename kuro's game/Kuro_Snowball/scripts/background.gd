ee3f4e5816fcf581738ec012b8cb7a9f7412f252
usagiKuro <chia0286@gmail.com>
2025-06-27 17:49:52 +0800

    Camera and Background
    - Updated camera to follow player
    - added background
    - updated to use a different gameover logic

--- kuro's game/Kuro_Snowball/scripts/background.gd
+++ kuro's game/Kuro_Snowball/scripts/background.gd
@@ -9,3 +9,21 @@ func _ready():
 	#$Ground/CollisionShape2D.position=Vector2(0.0,100)
 	$RightWall/CollisionShape2D.position=Vector2(srn.x,0.0)
 	
+	#display background image of the ground
+	$Ground_Image.position=Vector2(0.0,$Ground_Image.texture.get_height()/2)
+	#display background image above ground
+	$Bottom_Image.position=Vector2(0.0,-$Bottom_Image.texture.get_height()/2)
+	$Bottom_Image_2.position=Vector2(0.0,-$Bottom_Image.texture.get_height()-$Bottom_Image_2.texture.get_height()/2)
+	
+
+#if player is 1 image size above this image, move 2 image above
+#to make it seems continuous
+func update_pos(player_pos):
+	if ($Bottom_Image.position.y-$Bottom_Image.texture.get_height())>player_pos.y:
+		$Bottom_Image.position=Vector2(0.0,$Bottom_Image.position.y-$Bottom_Image_2.texture.get_height()-$Bottom_Image.texture.get_height())
+	if ($Bottom_Image_2.position.y-$Bottom_Image_2.texture.get_height())>player_pos.y:
+		$Bottom_Image_2.position=Vector2(0.0,$Bottom_Image_2.position.y-$Bottom_Image_2.texture.get_height()-$Bottom_Image.texture.get_height())
+		
+		
+	
+		
--- kuro's game/Kuro_Snowball/scripts/game.gd
+++ kuro's game/Kuro_Snowball/scripts/game.gd
@@ -12,24 +12,18 @@ var joystick_on
 var base_pos
 var dir
 
-var camera_y_start
-var camera_shift_interval
-var char_init_offset
-
 #scoring
 var cur_height
 var max_height
 var survival_time
+var player_base_height
 
 var snowball_base_spd #every 0.5 seconds 1 ball
 var snowball_spd_incr #each increment of difficulty minus 0.01 second
 var diff_incr_interval
 
 var game_over
-var last_player_pos
-var max_ticks_not_moving
-var tick_player_not_moving
-
+var overflow_snowball_limit
 
 var last_obstacle_y
 var obstacle_interval
@@ -41,18 +35,13 @@ var rnd
 func _ready():
 	#srn=DisplayServer.screen_get_size()
 	srn=DisplayServer.window_get_size()
-	#$Player.apply_scale(Vector2(50.0,50.0))
-	#$Player.position=Vector2(50.0,srn.y-10)
-	$Player.position=Vector2(50.0,-10)
+	#For player to fall onto the ground
+	$Player.global_position=Vector2(50.0,-10)
 	
 	force=20.0
 	max_spd=300.0
 	$Player/RigidBody2D.contact_monitor=true
 	$Player/RigidBody2D.max_contacts_reported=20; 
-	
-	#var dup=snowball.instantiate()
-	#dup.position=Vector2(100,0)
-	#add_child(dup)
 	
 	$Snowball_Timer.start()
 	
@@ -65,17 +54,16 @@ func _ready():
 	cur_height=0.0
 	max_height=0.0
 	survival_time=0.0
+	player_base_height=0.0
 	
-	#Start camera abit lower so ground can be seen
-	camera_y_start = srn.y-20.0 #The top left of camera
-	camera_shift_interval = 20 #smaller = smoother scrolling
-	$Camera2D.position=Vector2(srn.x/2,-camera_y_start/2)
-	char_init_offset=-1 #uninitialized
+	#Position camera	
+	$Camera2D.global_position=Vector2(srn.x/2,$Player/RigidBody2D.global_position.y)
+	#$Camera2D.offset=Vector2(0.0,-srn.y*1/3) #player 2/3 from top of screen
 	
 	#screen has shifted to slightly below ground so placing button on ground level is sufficient
 	#then move up with each offset
-	$JumpButton.position=Vector2(srn.x-10-$JumpButton.shape.radius,$Camera2D.offset.y-$JumpButton.shape.radius)
-	$Info.position=Vector2(0.0,-camera_y_start)
+	$JumpButton.position=Vector2(srn.x-10-$JumpButton.shape.radius , $Camera2D.global_position.y + $Camera2D.offset.y + srn.y/2 - $JumpButton.shape.radius)
+	$Info.position=Vector2(0.0,$Camera2D.global_position.y + $Camera2D.offset.y - srn.y/2)
 	$Info.text="Game Loading"
 	
 	
@@ -87,9 +75,8 @@ func _ready():
 	diff_incr_interval=3
 	game_over=false
 	
-	last_player_pos=Vector2($Player/RigidBody2D.global_position)
-	max_ticks_not_moving=10 #timer interval 0.5 sec so 5 sec is 10 ticks
-	tick_player_not_moving=0
+	#if stationary snowball exceeds this amount above screen, gameover
+	overflow_snowball_limit=10
 	
 	last_obstacle_y=0.0 #to hwlp limit obstacles placed
 	obstacle_interval=srn.y/3
@@ -130,8 +117,8 @@ func _process(delta):
 	
 func _on_snowball_timer_timeout() -> void:
 	var dup=snowball.instantiate()
-	#dup.position=Vector2(randf_range(0,srn.x-200),0)
-	dup.position=Vector2(randf_range(0,srn.x),-camera_y_start+$Camera2D.offset.y)
+	#Dropping from abit higher is fine
+	dup.position=Vector2(randf_range(0,srn.x) , $Camera2D.global_position.y + $Camera2D.offset.y - srn.y)
 	##Add speed to prevent detection of gameover
 	dup.find_child("RigidBody2D").linear_velocity=Vector2(0.0,0.01)
 	add_child(dup)
@@ -188,34 +175,34 @@ func cap_spd():
 		$Player/RigidBody2D.linear_velocity=Vector2($Player/RigidBody2D.linear_velocity.x,-max_spd)
 
 func move_screen():
-	#if player position is above mid screen, scroll screen up
-	if ($Player/RigidBody2D.position.y-char_init_offset) < ($Camera2D.offset.y-camera_shift_interval*14):
-		$Camera2D.offset=Vector2(0.0,$Camera2D.offset.y-camera_shift_interval/4)
-		$JumpButton.position=Vector2(srn.x-10-$JumpButton.shape.radius,$Camera2D.offset.y-$JumpButton.shape.radius)
-		$Info.position=Vector2(0.0,-camera_y_start+$Camera2D.offset.y)
-		
-		#if scroll screen up, check if should add an obstacle
-		if($Camera2D.offset.y<(last_obstacle_y-obstacle_interval) && rnd.randi()%2): #decide if should add obstacle
-			last_obstacle_y=$Camera2D.offset.y
-			var flip=rnd.randi()%2
-			var obstacle_x = 0.0
-			if(flip): #decide if should be left or right
-				obstacle_x=srn.x
-			$Obstacles.add_obstacle(rnd.randi()%6+1,Vector2(obstacle_x,-camera_y_start+$Camera2D.offset.y+200),flip)
+	#Move camera with player along the Y axis
+	$Camera2D.position=Vector2(srn.x/2,$Player/RigidBody2D.global_position.y)
+	$JumpButton.position=Vector2(srn.x-10-$JumpButton.shape.radius , $Camera2D.global_position.y + $Camera2D.offset.y + srn.y/2 - $JumpButton.shape.radius)
+	$Info.position=Vector2(0.0,$Camera2D.global_position.y + $Camera2D.offset.y - srn.y/2)
+	
+	$BackGround.update_pos($Player/RigidBody2D.position)
+	
+	var cur_pos_y=$Camera2D.global_position.y + $Camera2D.offset.y - srn.y/2
+	#if scroll screen up, check if should add an obstacle
+	if(cur_pos_y<(last_obstacle_y-obstacle_interval) && rnd.randi()%2): #decide if should add obstacle
+		last_obstacle_y=cur_pos_y
+		var flip=rnd.randi()%2
+		var obstacle_x = 0.0
+		if(flip): #decide if should be left or right
+			obstacle_x=srn.x
+		$Obstacles.add_obstacle(rnd.randi()%6+1,Vector2(obstacle_x, last_obstacle_y),flip)
 
 func _on_update_timer_timeout() -> void:
-	#Initialize distance
-	if char_init_offset==-1:
-		char_init_offset=$Player/RigidBody2D.position.y
-		
 	if game_over == true:
 		$Info.text+="\nGame Over"
 		$Snowball_Timer.stop()
 		$Update_Timer.stop()
 	
 	else:
+		if player_base_height==0.0:
+			player_base_height=$Player/RigidBody2D.global_position.y
 		#Display update
-		cur_height=snapped((char_init_offset-$Player/RigidBody2D.position.y)/10,0.01)
+		cur_height=-snapped(($Player/RigidBody2D.global_position.y-player_base_height)/10,0.01)
 		max_height=max(max_height,cur_height)
 		survival_time+=$Update_Timer.wait_time
 		
@@ -223,37 +210,38 @@ func _on_update_timer_timeout() -> void:
 		$Info.text+="Max Height: " + str(max_height) + " M\n"
 		$Info.text+="Survival Time: " + str(snapped(survival_time,1)) + " seconds"
 		
-		##debugging info
-		$Info.text+="\nActive_Movement: " + str(last_player_pos.distance_to($Player/RigidBody2D.global_position))
-		
 		#Difficulty up every 10 seconds
-		#but cap at 0.01 sec interval
-		if $Snowball_Timer.wait_time>0.01:
+		#but cap at 0.05 sec interval
+		if $Snowball_Timer.wait_time>0.05:
 			var spd=snowball_base_spd-snowball_spd_incr*snapped(survival_time/diff_incr_interval,1)
-			spd=max(0.01,spd) #cannot go below 0.01
+			spd=max(0.05,spd) #cannot go below 0.05 as per godot
 			$Snowball_Timer.wait_time=spd
-			
-		#chk if player moving
-		if(last_player_pos.distance_to($Player/RigidBody2D.global_position)<30.0):
-			tick_player_not_moving+=1
-		else:
-			tick_player_not_moving=0
-		last_player_pos = Vector2($Player/RigidBody2D.global_position)
-			
+						
 func check_gameOver():
-	var camera=$Camera2D.global_position+$Camera2D.offset
-	var srn_top_y=camera.y-srn.y/2 + 50
-	#if covered by snowballs up to depth of 1/3 the screen, assumed stuck
-	var player_buried_y = $Player/RigidBody2D.global_position.y-srn.y/3
+	var srn_top_y=$Camera2D.global_position.y+$Camera2D.offset.y-srn.y/2
+	var danger_zone=$Camera2D.global_position.y+$Camera2D.offset.y-srn.y/4
+	var stationary_count=0
 	
 	var snowballs = get_tree().get_nodes_in_group("new_snowballs")
 	for snball in snowballs:
-		if game_over == false && snball.find_child("RigidBody2D").linear_velocity.y < 0.0:
-			if snball.find_child("RigidBody2D").global_position.y < player_buried_y && tick_player_not_moving >= max_ticks_not_moving:
-				game_over=true
-			if snball.find_child("RigidBody2D").global_position.y < srn_top_y && tick_player_not_moving >= max_ticks_not_moving:
-				game_over=true
-			#snowballs that are stopped or bounced do not need to be checked anymore
-			if snball.find_child("RigidBody2D").global_position.y > $Player/RigidBody2D.position.y:
+		var ball=snball.find_child("RigidBody2D")
+		if game_over == false && ball.linear_velocity.y <= 0.0:
+			#nudge balls above the danger zone 
+			#to prevent balls stuck on cliffs
+			if ball.global_position.y < danger_zone:
+				#if left nudge right, else nudege left
+				if ball.global_position.x < srn.x/2 :
+					ball.apply_impulse(Vector2(1.0,0.0))
+				else:
+					ball.apply_impulse(Vector2(-1.0,0.0))
+			#if not moving and exceed top of screen then gameover
+			if ball.global_position.y < srn_top_y:
+				#game_over=true
+				stationary_count+=1
+			#snowballs below player are not application to gameover logic
+			if ball.global_position.y > $Player/RigidBody2D.position.y:
 				snball.remove_from_group("new_snowballs")
+				
+	if stationary_count>overflow_snowball_limit:
+		game_over=true
 		
