32862339f902a52de93c72d35a1c60eedb8976d2
Kushal Rana <kushalrana1420@gmail.com>
2025-06-30 16:41:22 +0545

    changed char and snowball sprites

--- kuro's game/Kuro_Snowball/scripts/player.gd
+++ kuro's game/Kuro_Snowball/scripts/player.gd
@@ -5,7 +5,6 @@ func _ready():
 	$RigidBody2D/AnimatedSprite2D.play("walk")
 	#$RigidBody2D.position=Vector2(10,srn.y-10)
 	#$RigidBody2D.position=Vector2(50,0)
-	$RigidBody2D/CollisionShape2D.scale=Vector2(0.25,0.25)
-	$RigidBody2D/AnimatedSprite2D.scale=Vector2(0.25,0.25)
+	#$RigidBody2D/CollisionShape2D.scale=Vector2(0.25,0.25)
+	#$RigidBody2D/AnimatedSprite2D.scale=Vector2(0.25,0.25)
 	#$RigidBody2D.scale=Vector2(5.0,5.0)
-	
